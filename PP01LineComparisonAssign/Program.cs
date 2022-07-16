fe8110c9a75f07ee225ae58d192d94707db78ba9
Hema <hemasahu019@gmail.com>
2022-07-16 12:23:08 +0530

    [Hema Added Welcome Page to Master]

---
+++ PP01LineComparisonAssign/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PP01LineComparisonAssign
+    /// 
+    ///Master - Welcome Page
+    ///
+{
+    internal class Program
+    {
+        //Entrypoint of console app
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to the PP01 Line Comparison Program Assignment ");
+            Console.ReadLine();
+        }
+    }
+}
